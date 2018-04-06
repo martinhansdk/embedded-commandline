38d7b86e8111dbe46e87eea1da7041bf0b516147
Brendan McDonnell <bmcdonnell@ionxlive.com>
2018-04-06 16:57:15 -0400

    replace spaces with tabs, to be consistent with other source files

--- src/CommandManager.h
+++ src/CommandManager.h
@@ -7,7 +7,7 @@
 namespace embeddedcommandline {
 
 int streq(const char *a, const char *b, int maxlen) {
-    while(maxlen > 0) {
+	while(maxlen > 0) {
 		if(*a != *b) {
 			return 0;
 		}
@@ -19,28 +19,28 @@ int streq(const char *a, const char *b, int maxlen) {
 		b++;
 		maxlen--;
 	}
-    return 1;
+	return 1;
 }
 
 
 template<int MAX_COMMANDS, int MAX_ARGS, int MAX_COMMAND_LEN>
 class CommandManager : public LineHandler {
-    Outputter &output;
+	Outputter &output;
 
 	const char* commandstr[MAX_COMMANDS];
-    Command* commands[MAX_COMMANDS];
-    int numberOfCommands;
+	Command* commands[MAX_COMMANDS];
+	int numberOfCommands;
 public:
-    CommandManager(Outputter& output) : output(output), numberOfCommands(0) {}
-    void addCommand(const char* str, Command& command) {
-        if(numberOfCommands < MAX_COMMANDS) {
+	CommandManager(Outputter& output) : output(output), numberOfCommands(0) {}
+	void addCommand(const char* str, Command& command) {
+		if(numberOfCommands < MAX_COMMANDS) {
 			commandstr[numberOfCommands] = str;
-            commands[numberOfCommands++] = &command;
-        }
-    }
+			commands[numberOfCommands++] = &command;
+		}
+	}
 
-    virtual void handleLine(char* line) {
-        const char* args[MAX_ARGS+1];
+	virtual void handleLine(char* line) {
+		const char* args[MAX_ARGS+1];
 
 		int pos=0;
 		int noOfArgs=0;
@@ -76,7 +76,7 @@ public:
 		output.puts("ERROR: Unknown command \"");
 		output.puts(line);
 		output.putchar('"');
-    }
+	}
 };
 
 
